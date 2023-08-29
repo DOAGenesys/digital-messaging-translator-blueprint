eae241118bfbcf639c356984218071593deaa123
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-08-29 09:47:01 +0200

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -11,5 +11,6 @@ export default {
         region: 'mypurecloud.ie'
     },
 
-    translateServiceURI: 'https://translate.eu-west-1.amazonaws.com'
+    translateServiceURI: 'https://digital-messaging-translator-blueprint.vercel.app/translate'
+    //translateServiceURI: 'https://translate.eu-west-1.amazonaws.com'
 } 
