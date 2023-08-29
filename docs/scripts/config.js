2fbbe645f85275e2cce74bdb17d574d8b3231eeb
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-08-29 19:36:50 +0200

    Delete config.js

--- docs/scripts/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    clientID: 'e279be86-0547-49e0-bb06-5e3fcf2c0107',
-
-    redirectUri: 'https://digital-messaging-translator-blueprint.vercel.app',
-
-    genesysCloud: {
-        region: 'mypurecloud.ie'
-    },
-} 
