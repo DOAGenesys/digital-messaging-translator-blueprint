a5c0f0daa9436754aaa147309c7b4ccec7c9b1ce
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-08-29 19:15:16 +0200

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -1,13 +1,9 @@
 export default {
-    // Token Implicit Grant Client ID
     clientID: 'e279be86-0547-49e0-bb06-5e3fcf2c0107',
 
     redirectUri: 'https://digital-messaging-translator-blueprint.vercel.app',
 
     genesysCloud: {
-        // Genesys Cloud region
-        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
-        // window.GCEnvironment
         region: 'mypurecloud.ie'
     },
 } 
